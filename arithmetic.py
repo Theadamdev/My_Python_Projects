def7af39832487ef34b10d3ecbbe7adba0525b1e
Theadamdev <adamsabri.student@gmail.com>
2025-07-21 11:33:08 +0100

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,14 @@
+a: int = int(input("Enter a number :"))
+b: int = int(input("Enter another number :"))
+
+def arithmetic(x:int , y:int) -> None :
+    print(f'The sum of {x} and {y} is {x + y}')
+    print(f'The difference of {x} and {y} is {x - y}')
+    print(f'The product of {x} and {y} is {x * y}')
+    print(f'The exponentation of {x} and {y} is {x ** y}')
+    if x != 0 :
+        print(f'The quotient of {x} and {y} is {x / y}')
+    else :
+        print('Sorry , Division by zero is not allowed')
+        
+arithmetic(a,b)
