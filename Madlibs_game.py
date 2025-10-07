21f5b2d10b2b27e2434c694023edb0df8dcf8024
Theadamdev <adamsabri.student@gmail.com>
2025-10-07 21:31:27 +0100

    Add files via upload

---
+++ Madlibs_game.py
@@ -0,0 +1,13 @@
+#Interresting madlibs game
+place = str(input("Enter a place : "))
+person = str(input("Enter a person : "))
+adverb = str(input("Enter an adverb: "))
+adjective = str(input("Enter an adjective : "))
+verb1 = str(input("Enter a verb ending in 'ing' : "))
+verb2 = str(input("Enter another verb ending in 'ing' : "))
+verb3 = str(input("Enter a third verb in the present simple : "))
+
+print(f"Today , I went to a {place} and saw a weird {person}")
+print(f"I was minding my own business when I saw him {verb1} {adverb}")
+print(f"I was so {adjective} that i started {verb2}")
+print(f"But it was all just a misunderstanding because {person} just wanted to {verb3} me")
