457aeb794584d746e96c3978bb1017cc0c9b4911
Theadamdev <adamsabri.student@gmail.com>
2025-10-08 21:33:37 +0100

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,3 +2,4 @@ print("Hello World , My name is Adam and I am a 16 year old highschool student i
 "I love programming and building things so in the next 3 years I will be able to build various" \
 "apps and maybe even my own game using all of these programming languages : Python / Java / Javascript /" \
 "HTMl / CSS and C++ . IF you don't believe me just wait and see :)")
+#This is me
