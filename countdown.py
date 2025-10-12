566a8ecd09752423632947797e99330361a808aa
Theadamdev <adamsabri.student@gmail.com>
2025-10-12 17:54:18 +0100

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -1,3 +1,4 @@
+#This is not fully completed , we still need to add a place where the user only enters the time in hours , minutes and seconds not only seconds 
 import time
 timer = int(input("Enter how many seconds you want to time : "))
 
@@ -7,5 +8,6 @@ for i in reversed(range(0, timer)):
     hours = int(i / 3600)
     print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
+
 
 print("TIME'S UP" )
