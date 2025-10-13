a04dad027b22e36cfcab2e8070516e93ce61c679
Theadamdev <adamsabri.student@gmail.com>
2025-10-13 21:10:42 +0100

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,7 +1,9 @@
+#this outputs a rectangle
 columns = int(input("How many columns do you want? "))
 rows = int(input("How many rows do you want? "))
 symbol = input("What symbol do you want? ")
 for i in range (rows) :
     for x in range (columns) :
         print(symbol , end = "")
+
     print(" ")
