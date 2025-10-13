2d36358a0b31708dd15cc5cf83be899ca88cfdda
Theadamdev <adamsabri.student@gmail.com>
2025-10-13 21:10:06 +0100

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,7 @@
+columns = int(input("How many columns do you want? "))
+rows = int(input("How many rows do you want? "))
+symbol = input("What symbol do you want? ")
+for i in range (rows) :
+    for x in range (columns) :
+        print(symbol , end = "")
+    print(" ")
