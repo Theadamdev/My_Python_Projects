2579a422316f89263fa2cc71798a277ca9bdd19b
Theadamdev <adamsabri.student@gmail.com>
2025-10-18 21:14:29 +0100

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,3 +1,4 @@
+#random dice
 import random
 name = str(input("What is your name? "))
 print(f"Hello {name}, we will play a game of dice together and from that we will get 3 random numbers")
@@ -6,4 +7,5 @@ number2 = random.randint(1,6)
 number3 = random.randint(1,6)
 print(f"Your first number is {number1}")
 print(f"Your second number is {number2}")
+
 print(f"Your third number is {number3}")
